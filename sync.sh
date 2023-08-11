6e9768a7eae356893fe4c365ed435e964b572843
charlzyx <charlzyx@qq.com>
2023-08-11 10:48:35 +0800

    auto update

---
+++ sync.sh
@@ -0,0 +1,14 @@
+rm -rf dnsmaq.conf
+rm -rf ./smb
+rm -rf ./lxc
+
+mkdir -p ./smb
+mkdir -p ./lxc
+
+cp /etc/dnsmasq.conf dnsmasq.conf
+cp /etc/smb/*.smb.conf ./smb/
+cp /titan/space/backup/pve/docker-compose.yaml ./docker-compose.yaml
+cp /etc/pve/lxc/*.conf ./lxc/
+
+git add .
+git commit -m 'auto update'
