81dbbfb2ebc7a86583b8111be5d6ed7e5a8a0854
charlzyx <charlzyx@qq.com>
2023-08-11 11:19:57 +0800

    auto update

--- sync.sh
+++ sync.sh
@@ -5,6 +5,7 @@ rm -rf ./lxc
 mkdir -p ./smb
 mkdir -p ./lxc
 
+cp ~/.zshrc .zshrc
 cp /etc/dnsmasq.conf dnsmasq.conf
 cp /etc/samba/*.conf ./smb/
 cp /titan/space/backup/pve/docker-compose.yaml ./docker-compose.yaml
