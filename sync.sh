8db9d7835b1c3d5eb4fdd27801d17015fee7b381
charlzyx <charlzyx@qq.com>
2023-08-16 09:53:33 +0800

    auto update

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ rm -rf ./lxc
 rm -rf ./sftpgo
 rm -rf ./dnsmasq
 rm -rf ./smb
+rm -rf ./systemd
 
 mkdir -p ./config
 mkdir -p ./docker
@@ -11,15 +12,21 @@ mkdir -p ./lxc
 mkdir -p ./sftpgo
 mkdir -p ./dnsmasq
 mkdir -p ./smb
+mkdir -p ./systemd
 
 cp ~/.zshrc ./config/
 cp ~/.config/nvim/init.vim  ./config/
 cp /titan/space/backup/pve/tailup.sh ./config/
+cp /etc/resolv.conf ./config/
+cp /etc/network/interfaces ./config/
+cp /etc/sysctl.conf ./config/
+
 cp /etc/pve/lxc/*.conf ./lxc/
-cp /etc/sftpgo/sftpgo.json ./
-cp /etc/dnsmasq.conf ./dnsmasq/dnsmasq.conf
+cp /etc/sftpgo/sftpgo.json ./sftpgo/
+cp /etc/dnsmasq.conf ./dnsmasq/
 cp /etc/samba/*.conf ./smb/
-cp /titan/space/backup/pve/docker-compose.yaml ./docker/docker-compose.yaml
+cp /titan/space/backup/pve/docker-compose.yaml ./docker/
+cp /etc/systemd/system/*.service ./systemd/
 
 git add .
 git commit -m 'auto update'
